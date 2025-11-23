488a83d49d19b66536694ebb535733a047a62dff
Guilherme Felix <guilhermerutemfelix@gmail.com>
2025-11-23 16:07:48 -0300

    fix(database): clear data

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,14 @@ module MessageMe
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     #
+    config.to_prepare do
+      # Garante que as chaves de ambiente sejam lidas
+      if ENV["ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY"]
+        config.active_record.encryption.primary_key = ENV["ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY"]
+        config.active_record.encryption.deterministic_key = ENV["ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY"]
+        config.active_record.encryption.key_derivation_salt = ENV["ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT"]
+        config.active_record.encryption.support_unencrypted_data = true
+      end
+    end
   end
 end
